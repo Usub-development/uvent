1f50ab86ac6cc431d646d1990960809727b00ed9
g2px1 <kir1kir1@yandex.ru>
2026-01-27 07:42:36 +0000

    fixed races

--- include/uvent/sync/AsyncSemaphore.h
+++ include/uvent/sync/AsyncSemaphore.h
@@ -4,100 +4,61 @@
 #include <atomic>
 #include <coroutine>
 #include <cstdint>
+
 #include "uvent/system/SystemContext.h"
 
 namespace usub::uvent::sync
 {
     class AsyncSemaphore
     {
+        enum class NodeState : uint8_t
+        {
+            Waiting = 0,
+            Cancelled = 1,
+            Claimed = 2
+        };
+
         struct WaitNode
         {
             std::coroutine_handle<> h{};
             WaitNode* next{};
+            int thread_id{};
+            std::atomic<NodeState> st{NodeState::Waiting};
         };
 
         std::atomic<int32_t> count_;
         std::atomic<WaitNode*> head_{nullptr};
 
-        static void resume_one(WaitNode* n) noexcept
+        static int get_thread_id(std::coroutine_handle<> h) noexcept
         {
-            system::co_spawn_static(n->h,
-                                    std::coroutine_handle<detail::AwaitableFrameBase>::from_address(n->h.address())
-                                        .promise()
-                                        .get_thread_id());
+            return std::coroutine_handle<detail::AwaitableFrameBase>::from_address(h.address())
+                .promise()
+                .get_thread_id();
         }
 
-    public:
-        explicit AsyncSemaphore(int32_t initial) noexcept : count_(initial) {}
-
-        struct AcquireAwaiter
+        void push_waiter(WaitNode* n) noexcept
         {
-            AsyncSemaphore* self;
-            WaitNode node{};
-
-            bool await_ready() noexcept
+            WaitNode* old = this->head_.load(std::memory_order_relaxed);
+            do
             {
-                int32_t c = this->self->count_.load(std::memory_order_relaxed);
-                while (c > 0)
-                {
-                    if (this->self->count_.compare_exchange_weak(c, c - 1, std::memory_order_acquire,
-                                                                 std::memory_order_relaxed))
-                        return true;
-                }
-                return false;
+                n->next = old;
             }
+            while (!this->head_.compare_exchange_weak(old, n, std::memory_order_release, std::memory_order_relaxed));
+        }
 
-            bool await_suspend(std::coroutine_handle<> h) noexcept
+        WaitNode* pop_waiter() noexcept
+        {
+            WaitNode* n = this->head_.load(std::memory_order_acquire);
+            while (n)
             {
-                this->node.h = h;
-                WaitNode* old = this->self->head_.load(std::memory_order_relaxed);
-                do
-                {
-                    this->node.next = old;
-                }
-                while (!this->self->head_.compare_exchange_weak(old, &this->node, std::memory_order_release,
-                                                                std::memory_order_relaxed));
-
-                int32_t c = this->self->count_.load(std::memory_order_acquire);
-                while (c > 0)
-                {
-                    if (this->self->count_.compare_exchange_weak(c, c - 1, std::memory_order_acquire,
-                                                                 std::memory_order_relaxed))
-                    {
-                        WaitNode* cur = this->self->head_.load(std::memory_order_acquire);
-                        WaitNode* prev = nullptr;
-                        while (cur)
-                        {
-                            if (cur == &this->node)
-                            {
-                                WaitNode* nxt = cur->next;
-                                if (prev)
-                                {
-                                    if (prev->next == cur)
-                                        prev->next = nxt;
-                                }
-                                else
-                                {
-                                    this->self->head_.compare_exchange_strong(cur, nxt, std::memory_order_acq_rel,
-                                                                              std::memory_order_relaxed);
-                                }
-                                return false;
-                            }
-                            prev = cur;
-                            cur = cur->next;
-                        }
-                        return false;
-                    }
-                }
-                return true;
+                WaitNode* next = n->next;
+                if (this->head_.compare_exchange_weak(n, next, std::memory_order_acq_rel, std::memory_order_relaxed))
+                    return n;
             }
+            return nullptr;
+        }
 
-            void await_resume() noexcept {}
-        };
-
-        AcquireAwaiter acquire() noexcept { return AcquireAwaiter{this}; }
-
-        bool try_acquire() noexcept
+        bool try_take_token() noexcept
         {
             int32_t c = this->count_.load(std::memory_order_relaxed);
             while (c > 0)
@@ -108,24 +69,64 @@ namespace usub::uvent::sync
             return false;
         }
 
+    public:
+        explicit AsyncSemaphore(int32_t initial) noexcept : count_(initial) {}
+
+        struct AcquireAwaiter
+        {
+            AsyncSemaphore* self{};
+            WaitNode node{};
+
+            bool await_ready() noexcept { return self->try_take_token(); }
+
+            bool await_suspend(std::coroutine_handle<> h) noexcept
+            {
+                this->node.h = h;
+                this->node.thread_id = get_thread_id(h);
+                this->node.st.store(NodeState::Waiting, std::memory_order_relaxed);
+
+                this->self->push_waiter(&this->node);
+
+                if (this->self->try_take_token())
+                {
+                    NodeState expected = NodeState::Waiting;
+                    if (node.st.compare_exchange_strong(expected, NodeState::Cancelled, std::memory_order_acq_rel,
+                                                        std::memory_order_relaxed))
+                        return false;
+                    return true;
+                }
+
+                return true;
+            }
+
+            void await_resume() noexcept {}
+        };
+
+        AcquireAwaiter acquire() noexcept { return AcquireAwaiter{this}; }
+
+        bool try_acquire() noexcept { return try_take_token(); }
+
         void release(int32_t k = 1) noexcept
         {
-            for (int i = 0; i < k; ++i)
+            for (int32_t i = 0; i < k; ++i)
             {
                 for (;;)
                 {
-                    WaitNode* n = this->head_.load(std::memory_order_acquire);
+                    WaitNode* n = pop_waiter();
                     if (!n)
-                        break;
-                    if (this->head_.compare_exchange_weak(n, n->next, std::memory_order_acq_rel,
-                                                          std::memory_order_relaxed))
                     {
-                        this->resume_one(n);
-                        goto next_i;
+                        this->count_.fetch_add(1, std::memory_order_release);
+                        break;
                     }
+
+                    NodeState expected = NodeState::Waiting;
+                    if (!n->st.compare_exchange_strong(expected, NodeState::Claimed, std::memory_order_acq_rel,
+                                                       std::memory_order_relaxed))
+                        continue;
+
+                    system::co_spawn_static(n->h, n->thread_id);
+                    break;
                 }
-                this->count_.fetch_add(1, std::memory_order_release);
-            next_i:;
             }
         }
     };
